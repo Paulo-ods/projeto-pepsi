9c0f0d329a04d57585dd27c95ecfe7c8c0b209d4
Paulo José dos Santos <paulo-santos_07@hotmail.com>
2025-10-16 20:50:42 -0300

    NavBar toggle

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let show = true;
 const menuContent = document.querySelector('.main-header-container');
 const menuToggle = menuContent.querySelector('.menu-toggle');
+const menuNav = menuContent.querySelector('.header-navigation-container');
 
 
 menuToggle.addEventListener('click', () => {
@@ -8,5 +9,6 @@ menuToggle.addEventListener('click', () => {
     document.body.style.overflow = show ? 'hidden' : 'initial'
 
     menuContent.classList.toggle('on', show);
+    menuNav.classList.toggle('on', show);
     show = !show;
 })
