7087947779aab0bc11d3bfd752d079415bfcf64d
Paulo José dos Santos <paulo-santos_07@hotmail.com>
2025-10-16 21:16:27 -0300

    teste borda

--- script.js
+++ script.js
@@ -12,3 +12,15 @@ menuToggle.addEventListener('click', () => {
     menuNav.classList.toggle('on', show);
     show = !show;
 })
+
+const header = document.querySelector('.main-header-container');
+const mainSection = document.querySelector('.main-section');
+
+function ajustarEspacamento() {
+  const alturaHeader = header.offsetHeight;
+  mainSection.style.marginTop = `${alturaHeader}px`;
+}
+
+// Executa ao carregar e ao redimensionar
+window.addEventListener('load', ajustarEspacamento);
+window.addEventListener('resize', ajustarEspacamento);
